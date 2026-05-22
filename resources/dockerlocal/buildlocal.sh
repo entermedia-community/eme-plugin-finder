492d4869a982cf479ffcc64ee6e4c94d5ea95cbb
Cristobal <cristobal.mejia@gmail.com>
2026-05-22 17:20:47 -0600

    app-emshare merge

--- resources/dockerlocal/buildlocal.sh
+++ resources/dockerlocal/buildlocal.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 DOCKERIMAGE=eme-lib-local  
 DOCKERNAME=emubuntutest
@@ -7,6 +7,8 @@ DOCKERNETWORK=entermedia
 
 sudo docker stop $DOCKERNAME
 sudo docker rm $DOCKERNAME	
+
+
 	
 sudo docker image rm $DOCKERIMAGE
 #sudo docker system prune
@@ -23,8 +25,22 @@ IP_ADDR="172.18.0.$NODENUMBER"
 
 ENDPOINT=../../../eme-server-test
 
+rm -rf "$ENDPOINT/*"
+
 USERID=$(id -u)
 GROUPID=$(id -g)
+
+
+# Create custom scripts
+SCRIPTROOT=${ENDPOINT}/bin
+
+echo "sudo docker start $INSTANCE" > ${SCRIPTROOT}/start.sh
+echo "sudo docker stop -t 60 $INSTANCE" > ${SCRIPTROOT}/stop.sh
+echo "sudo docker stop -t 60 $INSTANCE && sudo docker start $INSTANCE" > ${SCRIPTROOT}/restart.sh
+echo "sudo docker logs -f --tail 500 $INSTANCE"  > ${SCRIPTROOT}/logs.sh
+echo "sudo docker exec -it $INSTANCE bash"  > ${SCRIPTROOT}/bash.sh
+echo "sudo bash $SCRIPTROOT/eme-docker-init.sh $SITE $NODENUMBER" > ${SCRIPTROOT}/rebuild.sh
+echo 'sudo docker exec -it -u 0 '$INSTANCE' /usr/share/eme-lib/resources/docker/eme-docker-update.sh $1 $2' > ${SCRIPTROOT}/update.sh
 
 
 sudo docker run -t -d \
