8530688d33deda2ff9937f92680911cb9a45e42d
Cristobal Mejia <cristobal.mejia@gmail.com>
2026-09-10 13:42:46 -0600

    chat notifications

--- code/org/entermediadb/ai/skills/RenderLocalTemplateSkill.java
+++ code/org/entermediadb/ai/skills/RenderLocalTemplateSkill.java
@@ -30,7 +30,7 @@ public class RenderLocalTemplateSkill extends BaseSkill
 		// {
 		// response.setNextSkillEnabled(nextskill);
 		// }
-		log.info(response.getMessage());
+		// log.info(response.getMessage());
 		messageContext.setLastResponse(response);
 		// messageContext.log("sent" + response.getMessagePlain());
 
